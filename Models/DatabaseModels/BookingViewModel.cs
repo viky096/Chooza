8bc50c843eaefac02768b2403eed8de775a6cd90
Waleed421 <waleedahmed521@gmail.com>
2016-09-18 13:08:55 +0500

    Booking View Model

---
+++ Models/DatabaseModels/BookingViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DatabaseModels
+{
+    public class BookingViewModel
+    {
+        public Guid Booking_ID { get; set; }
+        public string Description { get; set; }
+        public string Date_Time { get; set; }
+        public string Status { get; set; }
+        public Guid Student_ID { get; set; }
+        public Guid Counsellor_ID { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public Guid CurrentApplicationId { get; set; }
+    }
+}
